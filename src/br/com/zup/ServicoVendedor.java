153377573ab767f71cdaebe05499e3ac709ed564
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 14:39:24 -0300

    Modelar classe serviços do vendedor

---
+++ src/br/com/zup/ServicoVendedor.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoVendedor {
+
+    private static List<Vendedor> vendedores = new ArrayList<>();
+
+}
