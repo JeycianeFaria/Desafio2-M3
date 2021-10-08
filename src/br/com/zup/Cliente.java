524e15e824a2152566617005161970a6469e2c2b
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 11:13:06 -0300

    Modelar classe Cliente

---
+++ src/br/com/zup/Cliente.java
@@ -0,0 +1,35 @@
+package br.com.zup;
+
+public class Cliente extends Pessoa {
+
+    private String telefone;
+
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf, String email, String telefone) {
+        super(nome, cpf, email);
+        this.telefone = telefone;
+    }
+
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder exibir = new StringBuilder();
+        exibir.append("======Cliente======");
+        exibir.append(super.toString());
+        exibir.append("\nTelefone: " + telefone);
+        return exibir.toString();
+    }
+
+}
