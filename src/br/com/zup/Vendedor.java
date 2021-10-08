7c6bac8470655a5f1cc94c5d7756d84d0a7f5a1c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 11:16:04 -0300

    Modelar classe Vendedor

---
+++ src/br/com/zup/Vendedor.java
@@ -0,0 +1,35 @@
+package br.com.zup;
+
+public class Vendedor extends Pessoa{
+
+    private double salario;
+
+
+    public Vendedor() {
+    }
+
+    public Vendedor(String nome, String cpf, String email, double salario) {
+        super(nome, cpf, email);
+        this.salario = salario;
+    }
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder exibir = new StringBuilder();
+        exibir.append("======Vendedor======");
+        exibir.append(super.toString());
+        exibir.append("\nSalário: R$" + salario);
+        exibir.append("====================");
+        return exibir.toString();
+    }
+}
