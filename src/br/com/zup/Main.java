c510661c8d799b9245c4d7ed2600b8b8e4bb1f9e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 13:54:12 -0300

    Criar classe Sistema

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente("Joao","142536","joao@joao", "9999999");
-        Vendedor vendedor = new Vendedor("Pedro", "968574", "pedro@pedro", 1500);
-        Venda venda = new Venda(cliente,vendedor,150,"08/10");
-
-        System.out.println(venda);
+        
 
     }
 }
