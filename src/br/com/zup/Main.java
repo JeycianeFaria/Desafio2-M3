83cb6194c4b1553758026612f6245c5a6bd01f84
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 15:08:33 -0300

    Modelar classe serviço de vendas

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         try {
             Sistema.cadastrarVendedor();
+            Sistema.cadastrarVendedor();
             ServicoVendedor.exibirVendedoresCadastrados();
+            System.out.println(Sistema.buscarVendedor());
         }catch (Exception erro){
             System.out.println(erro.getMessage());
         }
---
+++ src/br/com/zup/ServicoVenda.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoVenda {
+
+    private  static List<Venda> registroVendas = new ArrayList<>();
+
+}
