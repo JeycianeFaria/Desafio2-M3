0572060a270c1dfe434cd231a796cfd9db6dc79a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-08 13:55:34 -0300

    Adicionar método para capturar dados do usuário

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,7 +1,14 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
 
+    public static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
 
+        return new Scanner(System.in);
+
+    }
 
 }
