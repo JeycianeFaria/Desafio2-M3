19e94265dfee032f109a884aa3e620c46fc534d0
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-11 09:36:52 -0300

    Adicionar método menu gerenciar cliente

--- src/br/com/zup/SistemaCliente.java
+++ src/br/com/zup/SistemaCliente.java
@@ -2,29 +2,40 @@ package br.com.zup;
 
 import java.util.List;
 
+import static br.com.zup.Sistema.capturarDados;
+
 public class SistemaCliente {
 
+    public static void menuCliente(){
+        System.out.println("\n================MENU CLIENTE=================");
+        System.out.println("Digite 1 - para cadastrar um cliente.");
+        System.out.println("Digite 2 - para exibir clientes cadastrados.");
+        System.out.println("Digite 3 - para exibir todas as compras por cliente.");
+        System.out.println("Digite 5 - para voltar ao menu principal.");
+    }
+
+
     public static Cliente cadastrarCliente() throws Exception{
-        String nome = Sistema.capturarDados("Digite o nome do cliente: ").nextLine();
-        String cpf = Sistema.capturarDados("Digite o cpf do cliente: ").nextLine();
+        String nome = capturarDados("Digite o nome do cliente: ").nextLine();
+        String cpf = capturarDados("Digite o cpf do cliente: ").nextLine();
         ServicoCliente.verificarCpfCliente(cpf);
-        String email = Sistema.capturarDados("Digite o email do cliente: ").nextLine();
+        String email = capturarDados("Digite o email do cliente: ").nextLine();
         ServicoCliente.validarEmail(email);
         ServicoCliente.verificarEmailCliente(email);
-        String telefone = Sistema.capturarDados("Digite o telefone do cliente: ").nextLine();
+        String telefone = capturarDados("Digite o telefone do cliente: ").nextLine();
 
         return ServicoCliente.cadastrarCliente(nome, cpf, email, telefone);
     }
 
 
     public static Cliente buscarCliente() throws Exception {
-        String cpfBusca = Sistema.capturarDados("Digite o CPF do cliente que deseja buscar: ").nextLine();
+        String cpfBusca = capturarDados("Digite o CPF do cliente que deseja buscar: ").nextLine();
         return ServicoCliente.buscarCliente(cpfBusca);
     }
 
 
     public static List<Venda> comprasPorCliente() throws Exception{
-        String cpf = Sistema.capturarDados("Digite o cpf do cliente que deseja verificar as compras: ").nextLine();
+        String cpf = capturarDados("Digite o cpf do cliente que deseja verificar as compras: ").nextLine();
         Cliente cliente = ServicoCliente.buscarCliente(cpf);
 
         return ServicoVenda.comprasPorCliente(cliente);
