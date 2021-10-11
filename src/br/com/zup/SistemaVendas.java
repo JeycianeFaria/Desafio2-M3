b94d5fba403a8332800b3b23c3745c64b475e733
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-11 10:01:27 -0300

    Refatorar adicionando método menu vendas

--- src/br/com/zup/SistemaVendas.java
+++ src/br/com/zup/SistemaVendas.java
@@ -4,6 +4,16 @@ import static br.com.zup.Sistema.capturarDados;
 
 public class SistemaVendas {
 
+    public static void menuVendas(){
+        System.out.println("\n================MENU VENDAS==================");
+        System.out.println("Digite 1 - para registar uma venda.");
+        System.out.println("Digite 2 - para exibir vendas registradas.");
+        System.out.println("Digite 3 - para exibir todas as compras por cliente.");
+        System.out.println("Digite 4 - para exibir todas as vendas de um vendedor.");
+        System.out.println("Digite 5 - para voltar ao menu principal.");
+    }
+
+
     public static Venda registrarVenda() throws Exception {
         Cliente cliente = SistemaCliente.buscarCliente();
         Vendedor vendedor = SistemaVendedor.buscarVendedor();
